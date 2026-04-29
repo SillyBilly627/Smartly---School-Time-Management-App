8ec2d0bad4bd021ac0856ae4186a81523e21c82f
Xavier <xavier172009@outlook.com.au>
2026-04-29 14:34:40 +0800

    Initial Commit

---
+++ Smartly/SmartlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartlyApp.swift
+//  Smartly
+//
+//  Created by xavier schulz on 29/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct SmartlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
